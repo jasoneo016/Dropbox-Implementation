c17fe76918407275e16712bc34f464ab3460f3e6
Jason Zhang <jasoneo016@gmail.com>
2017-01-25 01:18:25 -0800

    changed port #

--- Dropbox.js
+++ Dropbox.js
@@ -85,6 +85,6 @@ function deleteFileFromS3(filePath, fileName) {
     });
 }
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(3300, function () {
+    console.log('Example app listening on port 3300!')
 })
